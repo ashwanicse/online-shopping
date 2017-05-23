0cc281f29a816e01ed26d80d3f4eb38343736076
ashwanijava <ashwanicse@outlook.com>
2017-05-24 01:52:55 +0530

    RequestParam annotation usage is tested successfully.

--- onlineshopping/src/main/java/com/niit/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/niit/onlineshopping/controller/PageController.java
@@ -2,6 +2,7 @@ package com.niit.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,6 +13,18 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to spring webmvc");
 		return mv;
-	}	
+	}
+	
+	// http://localhost:8081/onlineshopping/test
+	// http://localhost:8081/onlineshopping/test?greeting=Welcome to spring webmvc RequestParam example
+	@RequestMapping(value = "/test")
+	//public ModelAndView test(@RequestParam("greeting") String greeting) {
+	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
+		if(greeting==null)
+			greeting="Hey, it is default message";
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
 
 }
